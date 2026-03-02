a44e67382c216a1139a2956a33984d7fa20e5cbd
upybunai-lang <upybunai@gmail.com>
2026-03-03 02:32:15 +0700

    Create build-static.php to compile Blade templates for deployment

---
+++ build-static.php
@@ -0,0 +1,13 @@
+<?php
+// build-static.php
+
+// Function to compile Blade templates to static HTML
+function compileBladeTemplates() {
+    // Add logic to compile Blade templates
+    // This is a placeholder for your compilation logic
+    echo 'Compiling Blade templates...';
+}
+
+// Execute the function
+compileBladeTemplates();
+?>
